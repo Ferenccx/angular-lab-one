e06f85de0d7f08d671ad70c034a88af1f4d437d4
Ferenccx <69224300+Ferenccx@users.noreply.github.com>
2021-01-19 22:10:12 -0700

    updates

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,50 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-//must include a Todo Interface
-interface Todo {
-  task: string;
-  completed: boolean;
-
+import { Component, OnInit } from '@angular/core';
+// Create Todo interface
+interface Todo {
+  task: string,
+  completed: boolean
 }
-// must include a component call "to-do"
-
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
 })
 
-//component must add an array of sample todo objects
-export class TodoComponent implements OnInit {
-  todos : Todo [] = [
+export class TodoComponent implements OnInit {
+  todos : Todo [] = [
+    {task: 'Feed Cats', completed: true},
+    {task: 'Clean Out Dishwasher', completed: true},
+    {task: 'exercise', completed: true},
+    {task: 'do laundry', completed: false},
+    {task: 'make a cup of tea', completed: true},
+    {task: 'write down three items that I want to get done today', completed: true},
+  
+  ]
+//   constructor() { }
 
-    {task: 'Feed Cats', completed: true},
-    {task: 'Clean Out Dishwasher', completed: true},
-    {task: 'exercise', completed: true},
-    {task: 'do laundry', completed: false},
-    {task: 'make a cup of tea', completed: true},
-    {task: 'write down three items that I want to get done today', completed: true},
-  
-  ]
-  constructor() { }
+  public newTask:string; 
+  public filterString:string = "";
+  completeTask = function(todo: Todo): void {
+      todo.completed = true;
+  }
+  deleteTask = function(todo: Todo): void {
+    let i = this.todo.findIndex(obj => obj.task === todo.task);
+    this.todo.splice(i,1);
+  }
+  addToList = function(newTask): void{
+    console.log("adding");
+    if (this.newTask.trim() != '') { 
+      let newTodo:Todo = {task: this.newTask,completed:false};
+      this.todo.push(newTodo);
+      this.newTask = '';
+    }
+  }
+  getFilteredResults(): Todo[] {
+    return this.todos.filter((todo => {
+      return todo.task.toLowerCase().includes(this.filterString.toLowerCase());
+    }));
+  }
 
-  ngOnInit(): void {
-  }
-  //create a new function
-completetask(todo){
-  todo.completed=true
+  constructor() { }
+  ngOnInit(): void {
+  }
 }
-}
-//getFilteredResults
-
-// //getFilteredResults(): Todo[] {
-//   return this.displayedTodos.filter((todo) => {
-//     // convert the todo.task to lowercase
-//     const taskLower = todo.task.toLowerCase();
-//     // convert the filter input to lowercase
-//     const filterLower = this.filter.toLowerCase();
-//     // check to set if the filter input is inside of the task
-//     // string
-//     return taskLower.includes(filterLower);
-//   });
-// }
-// //
